07f4c9d104368b44505803844c52915d4a9c168a
pnowak86 <p_nowak86@poczta.fm>
2017-07-05 20:06:51 +0200

    second commit

--- src/main/java/com/sda/word/Countable.java
+++ src/main/java/com/sda/word/Countable.java
@@ -4,5 +4,8 @@ package com.sda.word;
  * Created by RENT on 2017-07-05.
  */
 public interface Countable {
-    
+
+
+    int countLines(String filePath);
+
 }
--- src/main/java/com/sda/word/Demo.java
+++ src/main/java/com/sda/word/Demo.java
@@ -3,6 +3,8 @@ package com.sda.word;
 public class Demo {
 
     public static void main(String[] args) {
+CountLines cs = new CountLines();
+        System.out.println("Number of lines in file: " + cs.countLines("file.txt"));
 
     }
 }
