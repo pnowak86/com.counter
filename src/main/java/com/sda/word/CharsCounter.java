6116fdbfd782eb63bf9be7881b76401ac011dce4
MBaran88 <zdzibq@gmail.com>
2017-07-05 19:42:03 +0200

    masa rzeczy dodana

--- src/main/java/com/sda/word/CharsCounter.java
+++ src/main/java/com/sda/word/CharsCounter.java
@@ -4,22 +4,43 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by RENT on 2017-07-05.
  */
-public class CharsCounter implements Countable{
+public class CharsCounter implements Countable {
 
 
     @Override
     public int count(String filePath) {
-        List<String> chars = null;
+        int ch;
+        int charsCount = 0;
+        int wordsCount = 0;
+        boolean prev = true;
+        //List<String> chars = null;
         try {
-            chars = Files.readAllLines(Paths.get(filePath));
-
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            System.out.println(lines.get(0));
+            lines.toString().replaceAll(" ","");
+            return lines.stream().mapToInt(String::length).sum();
+            //List<String> charCounter = lines.stream().map
         } catch (IOException e) {
             e.printStackTrace();
         }
         return 0;
+    }}
+        /*
+        Scanner in = new Scanner(filePath);
+        //while (in.hasNextLine()){
+        //String line = in.nextLine();
+        //Scanner lineScanner = new Scanner(line);
+        while (in.hasNext()) {
+            wordsCount++;
+            String word = in.next();
+            charsCount += word.length();
+        }
+        in.close();
+        return charsCount;
     }
-}
+}*/
--- src/main/java/com/sda/word/Demo.java
+++ src/main/java/com/sda/word/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 
     public static void main(String[] args) {
         CharsCounter charsCounter = new CharsCounter();
+        System.out.println(charsCounter.count("file.txt"));
 
     }
 }
